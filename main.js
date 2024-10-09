200e0213c7663348bdcf73b29ced3d222df35972
Seif Samir <117075048+seif-samir89@users.noreply.github.com>
2024-10-09 17:53:28 +0300

    CodeCrafter

---
+++ main.js
@@ -0,0 +1,36 @@
+AOS.init({
+  duration: 1000,
+  once: true,
+  offset: 200,
+});
+//
+document.addEventListener("DOMContentLoaded", function () {
+  let projectFilters = document.querySelectorAll("#project-flters li");
+  let projectItems = document.querySelectorAll(".project-item");
+
+  projectFilters.forEach((filter) => {
+    filter.addEventListener("click", function () {
+      // Remove active class from all filters
+      projectFilters.forEach((filter) =>
+        filter.classList.remove("filter-active")
+      );
+
+      // Add active class to the clicked filter
+      this.classList.add("filter-active");
+
+      let filterValue = this.getAttribute("data-filter");
+
+      // Show/Hide project items based on filter
+      projectItems.forEach((item) => {
+        if (
+          filterValue === "*" ||
+          item.classList.contains(filterValue.slice(1))
+        ) {
+          item.style.display = "block";
+        } else {
+          item.style.display = "none";
+        }
+      });
+    });
+  });
+});
